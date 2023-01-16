31ca26158d0252d613cac4796ff1586e7c17fc7e
Etienne ALEXANDRE <tietienne77@gmail.com>
2023-01-16 18:31:26 +0100

    tests : add simple test

--- BimIot-Back/src/test/java/com/example/backbimiot/BackBimiotApplicationTests.java
+++ BimIot-Back/src/test/java/com/example/backbimiot/BackBimiotApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.backbimiot;
 
+import com.example.backbimiot.websocket.SubscribeMappingController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class BackBimiotApplicationTests {
 
+    @Autowired
+    private SubscribeMappingController sub;
+
     @Test
-    void contextLoads() {
+    public void subscribeTest() {
+        var answer = "server one-time message via the application";
+        assertEquals(answer, sub.sendOneTimeMessage());
     }
 
 }
